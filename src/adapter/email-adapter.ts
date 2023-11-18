341f6f8a1ea9ed18a06a06e695d032c01eb4cbb2
MihPar <9496260@gmail.com>
2023-11-18 13:07:56 +0300

    fix1307181123

--- src/adapter/email-adapter.ts
+++ src/adapter/email-adapter.ts
@@ -15,9 +15,9 @@ export const emailAdapter = {
 			  from: 'Mihail <mpara7274@gmail.com>', // sender address
 			  to: email, // list of receivers
 			  subject: 'Hello worlds', // Subject line
-			  html: `<h1>Thank for your registration</h1>
-			  <p>To finish registration please follow the link below:
-				  <a href='https://somesite.com/password-recovery?recoveryCode=${code}'>complete registration</a>
+			  html: `<h1>Password recovery</h1>
+			  <p>To finish password recovery please follow the link below:
+			  <a href='https://somesite.com/password-recovery?recoveryCode=${code}'>recovery password</a>
 			  </p>`
 			});
 		  }
