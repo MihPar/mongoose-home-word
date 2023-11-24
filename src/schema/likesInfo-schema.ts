53f39f2005a114e8f61839d1028a40ab26bcae41
MihPar <9496260@gmail.com>
2023-11-24 17:46:25 +0300

    fix2311241746

--- src/schema/likesInfo-schema.ts
+++ src/schema/likesInfo-schema.ts
@@ -3,8 +3,12 @@ import mongoose from "mongoose";
 import { LikeStatusEnum } from "../enum/like-status-enum";
 import { LikesInfo } from "../types/likesInfoType";
 
-export const LikesInfoSchema = new mongoose.Schema<WithId<LikesInfo>> ({
-	userId: {type: mongoose.Schema.Types.ObjectId, required: true},
-	commentId: {type: String, required: true},
-    myStatus: LikeStatusEnum
-})
+export const LikesInfoSchema = new mongoose.Schema<WithId<LikesInfo>>({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  commentId: { type: String, required: true },
+  myStatus: {
+    type: String,
+    default: LikeStatusEnum.None,
+    enum: Object.values(LikeStatusEnum)
+  },
+});
