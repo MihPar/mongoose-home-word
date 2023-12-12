23fa38ebf101b863e40e7b951d7f50fb2085b4bd
MihPar <9496260@gmail.com>
2023-12-12 14:12:49 +0300

    fix2311121412

--- src/middleware/commentAuthorization.ts
+++ src/middleware/commentAuthorization.ts
@@ -3,6 +3,7 @@ import { HTTP_STATUS } from "../utils/utils";
 import { jwtService } from "../Compositions-root/auth-composition-root";
 import { UsersModel } from "../db/db";
 import { ObjectId } from "mongodb";
+import { queryUsersRepositories } from "../Compositions-root/user-composition-root";
 
 export const commentAuthorization = async function(req: Request, res: Response, next: NextFunction) {
 	if(!req.headers.authorization) {
@@ -10,21 +11,21 @@ export const commentAuthorization = async function(req: Request, res: Response, 
 	}
 	const token = req.headers.authorization.split(' ')[1]
 	const userId = await jwtService.getUserIdByToken(token)
-	if(!userId) {
-		return res.sendStatus(HTTP_STATUS.NOT_AUTHORIZATION_401)
-	}
-	req.user = await UsersModel.findOne({ _id: new ObjectId(userId) })
-	return next()
-
-	// if(userId) {
-	// 	const resultAuth = await queryUsersRepositories.findUserById(userId)
-	// 	if(resultAuth){
-	// 		req.user = resultAuth
-	// 		next()
-	// 		return 
-	// 	}
-	// 	return res.sendStatus(HTTP_STATUS.NOT_AUTHORIZATION_401)
-	// } else {
+	// if(!userId) {
 	// 	return res.sendStatus(HTTP_STATUS.NOT_AUTHORIZATION_401)
 	// }
+	// req.user = await UsersModel.findOne({ _id: new ObjectId(userId) })
+	// return next()
+
+	if(userId) {
+		const resultAuth = await queryUsersRepositories.findUserById(userId)
+		if(resultAuth){
+			req.user = resultAuth
+			next()
+			return 
+		}
+		return res.sendStatus(HTTP_STATUS.NOT_AUTHORIZATION_401)
+	} else {
+		return res.sendStatus(HTTP_STATUS.NOT_AUTHORIZATION_401)
+	}
 }
